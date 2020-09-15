497a20635b5e4c8200dc79e6d7c25912e6e2bcb6
smb-lsp <smb-lsp@smb-lspdeMac-mini.local>
2020-09-15 13:35:18 +0800

    init

---
+++ MapLike/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapLike
+//
+//  Created by smb-lsp on 2020/9/15.
+//  Copyright © 2020 SMB-APP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapLike/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapLike
+//
+//  Created by smb-lsp on 2020/9/15.
+//  Copyright © 2020 SMB-APP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
